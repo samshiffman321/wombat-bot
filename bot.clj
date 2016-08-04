405b0ceacde253d0493cd420829b036dd131f8b5
Sam Shiffman <samshiffman321@gmail.com>
2016-08-04 09:37:14 -0400

    Update bot

--- bot.clj
+++ bot.clj
@@ -2,7 +2,7 @@
   [{:keys  [arena state bot_id energy spawn-bot?] :as step-details}]
   (let [command-options [
                          [{:cmd  "SHOOT"
-                           :metadata  {:direction (rand-nth  [0 1 2 3 4 5 6 7]) :energy 20}}]
+                           :metadata  {:direction (rand-nth  [1 3 5 7]) :energy 20}}]
                          [{:cmd  "MOVE"
                            :metadata  {:direction (rand-nth  [0 1 2 3 4 5 6 7])}}]]]
     {:commands (rand-nth command-options)}))
